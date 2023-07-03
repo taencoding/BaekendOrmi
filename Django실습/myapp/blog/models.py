b8ea0031b9fc70483e6888fcc5fd8275e982fffe
taencoding <xodms512@gmail.com>
2023-07-03 17:37:49 +0900

    Django Day7

--- Django실습/myapp/blog/models.py
+++ Django실습/myapp/blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    writer = models.CharField(max_length=10)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE) # 작성자와 글은 1:N 의 관계, 1이 삭제되면 N이 삭제되도록 CASCADE
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
