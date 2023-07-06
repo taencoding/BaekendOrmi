2ee7e80586ffd29528c85101efec4684b476ddc1
taencoding <xodms512@gmail.com>
2023-07-06 18:02:42 +0900

    Django Day10

--- Django실습/myapp/blog/views.py
+++ Django실습/myapp/blog/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views import View
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from .models import Post, Comment, HashTag
 from .forms import PostForm, CommentForm, HashTagForm
 from django.urls import reverse_lazy, reverse
@@ -22,7 +23,7 @@ class Index(View):
         
         # 데이터베이스에 접근해서 값을 가져와야 합니다.
         # 게시판에 글들을 보여줘야하기 때문에 데이터베이스에서 "값 조회"
-        # MyModel.objects.all()
+        # MyModel.objects.all() SELECT * from post
         post_objs = Post.objects.all()
         # context = 데이터베이스에서 가져온 값
         context = {
@@ -132,7 +133,13 @@ class Write(LoginRequiredMixin, View):
     
 class Update(View):
     def get(self, request, pk): # pk는 post_id
-        post = Post.objects.get(pk=pk)
+        # try, except
+        try:
+            post = Post.objects.get(pk=pk) # <Object: post>
+        # get()은 해당 조건이 없을 때 오류를 발생시킨다.
+        except ObjectDoesNotExist as e:
+            print('Post does not exist', str(e))
+
         form = PostForm(initial={'title': post.title, 'content': post.content})
         context = {
             'form': form,
@@ -142,7 +149,12 @@ class Update(View):
         return render(request, 'blog/post_edit.html', context)
     
     def post(self, request, pk):
-        post = Post.objects.get(pk=pk)
+        ## try, execpt
+        try:
+            post = Post.objects.get(pk=pk)
+        except ObjectDoesNotExist as e:
+            print('Post does not exist', str(e))
+
         form = PostForm(request.POST)
         if form.is_valid():
             post.title = form.cleaned_data['title']
@@ -150,7 +162,7 @@ class Update(View):
             post.save()
             return redirect('blog:detail', pk=pk) # pk는 post_id
         
-        form.add_error('폼이 유효하지 않습니다.')
+        # form.add_error('폼이 유효하지 않습니다.')
         context = {
             'form': form,
             'title': 'Blog'
@@ -165,7 +177,12 @@ class Update(View):
 
 class Delete(View):
     def post(self, request, pk): # post_id
-        post = Post.objects.get(pk=pk)
+        ## try, execpt
+        try:
+            post = Post.objects.get(pk=pk)
+        except ObjectDoesNotExist as e:
+            print('Post does not exist', str(e))
+
         post.delete()
         return redirect('blog:list')
     
@@ -181,11 +198,28 @@ class DetailView(View):
         # 데이터베이스 방문
         # 해당 글
         # 장고 ORM (pk: 무조건 pk로 작성해야한다.)
-        post = Post.objects.get(pk=pk)
+        # post = Post.objects.get(pk=pk)
+        # # 댓글
+        # comments = Comment.objects.filter(post=post)
+        # # 해시태그
+        # hashtags = HashTag.objects.filter(post=post)
+        # print(post)
+        
         # 댓글
-        comments = Comment.objects.filter(post=post)
+        # comments = Comment.objects.select_related('writer').filter(post=post)
+        # comments = Comment.objects.select_related('writer').filter(post__pk=pk)
+        comments = Comment.objects.select_related('post') # -> comments[0]
+        # comment = Comment.objects.select_related('post').first()
         # 해시태그
-        hashtags = HashTag.objects.filter(post=post)
+        # hashtags = HashTag.objects.select_related('writer').filter(post=post)
+        # hashtags = HashTag.objects.select_related('writer').filter(post__pk=pk)
+        hashtags = HashTag.objects.select_related('post')
+        # print(comments[0].post.title)
+        # for comment in comments:
+        #     print(comment.post)
+        # <QuerySet[]>
+        # value.attr
+        # print(hashtags)
         
         # 댓글 Form
         comment_form = CommentForm()
@@ -194,8 +228,12 @@ class DetailView(View):
         hashtag_form = HashTagForm()
         
         context = {
-            'title': 'Blog',
-            'post': post,
+            "title": "Blog",
+            'post_id': pk,
+            'post_title': comments[0].post.title,
+            'post_content': comments[0].post.content,
+            'post_writer': comments[0].post.writer,
+            'post_created_at': comments[0].post.created_at,
             'comments': comments,
             'hashtags': hashtags,
             'comment_form': comment_form,
@@ -206,13 +244,24 @@ class DetailView(View):
 
 
 ### Comment
-class CommentWrite(View):
+class CommentWrite(LoginRequiredMixin, View):
     # def get(self, request):
     #     pass
+    '''
+    1. LoginRequiredMixin -> 삭제
+    2. 비회원 유저 권한 User
+    '''
     def post(self, request, pk):
         form = CommentForm(request.POST)
         # 해당 아이디에 해당하는 글 불러옴
-        post = Post.objects.get(pk=pk)
+        ## try, except
+        try:
+            post = Post.objects.get(pk=pk) # <Object: post>
+        # get()은 해당 조건이 없을 때 오류를 발생시킨다.
+        except ObjectDoesNotExist as e:
+            print('Post does not exist', str(e))
+        # get 관련 쿼리들은 해당 데이터가 없을 때 오류 발생
+        # get_or_404
         
         if form.is_valid():
             # 사용자에게 댓글 내용을 받아옴
@@ -220,8 +269,21 @@ class CommentWrite(View):
             # 유저 정보 가져오기
             writer = request.user
             # 댓글 객체 생성, create 메서드를 사용할 때는 save 필요 없음
-            comment = Comment.objects.create(post=post, content=content, writer=writer)
-            # comment = Comment(post=post) -> comment.save()
+            try:
+                comment = Comment.objects.create(post=post, content=content, writer=writer)
+                # 생성할 값이 이미 있다면 오류 발생, Unique 값이 중복될 때
+                # 필드 값이 비어있을 때: ValidationError
+                # 외래키 관련 데이터베이스 오류 : ObjectDoesNotExist
+                # get_or_create() -> 2가지 경우의 리턴값
+                # comment, created = Comment.objects.get_or_create(post=post, content=content, writer=writer)
+                # if created: print('생성되었습니다.') else: print('이미 있습니다.')
+                # comment = Comment(post=post) -> comment.save()
+            except ObjectDoesNotExist as e:
+                print('Post does not exist', str(e))
+
+            except ValidationError as e:
+                print('Validation error occurred', str(e))
+
             return redirect('blog:detail', pk=pk)
         
         # form.add_error('comment_error', '폼이 유효하지 않습니다.')
@@ -230,7 +292,8 @@ class CommentWrite(View):
         hashtag_form = HashTagForm()
         context = {
             "title": "Blog",
-            'post': post,
+            'post_id': pk,
+            # post.title, post.content
             'comments': post.comment_set.all(),
             'hashtags': post.hashtag_set.all(),
             'comment_form': form,
@@ -242,7 +305,12 @@ class CommentWrite(View):
 class CommentDelete(View):
     def post(self, request, pk):
         # 지울 객체를 찾아야 한다. -> 댓글 객체
-        comment = Comment.objects.get(pk=pk)
+        ## try, exept
+        try:
+            comment = Comment.objects.get(pk=pk)
+        except ObjectDoesNotExist as e:
+            print('Comment does not exist', str(e))
+
         # 상세페이지로 돌아가기
         post_id = comment.post.id
         # 삭제
@@ -252,23 +320,36 @@ class CommentDelete(View):
 
 
 ### Tag
-class HashTagWrite(View):
+class HashTagWrite(LoginRequiredMixin, View):
     def post(self, request, pk): # pk는 post_id
         form = HashTagForm(request.POST)
         # 해당 아이디에 해당하는 글 불러옴
-        post = Post.objects.get(pk=pk)
+        ## try, execpt
+        try:
+            post = Post.objects.get(pk=pk)
+        except ObjectDoesNotExist as e:
+            print('Comment does not exist', str(e))
 
         if form.is_valid():
             # 사용자에게 태그 내용을 받아옴
             name = form.cleaned_data['name']
             # 작성자 정보 가져오기
             writer = request.user
+
             # 댓글 객체 생성, create 메서드를 사용할 때는 save 필요 없음
-            hashtag = HashTag.objects.create(post=post, name=name, writer=writer)
+            try:
+                hashtag = HashTag.objects.create(post=post, name=name, writer=writer)
+            
+            except ObjectDoesNotExist as e:
+                print('Comment does not exist', str(e))
+            
+            except ValidationError as e:
+                print('Validation error occurred', str(e))
+
             # comment = Comment(post=post) -> comment.save()
             return redirect('blog:detail', pk=pk)
         
-        form.add_error(None, '폼이 유효하지 않습니다.')
+        # form.add_error(None, '폼이 유효하지 않습니다.')
         comment_form = CommentForm()
 
         context = {
@@ -285,7 +366,12 @@ class HashTagWrite(View):
 class HashTagDelete(View):
     def post(self, request, pk): # pk는 hashtag_id
         # 지울 객체를 찾아야 한다. -> 태그 객체
-        hashtag = HashTag.objects.get(pk=pk)
+        try:
+            hashtag = HashTag.objects.get(pk=pk)
+            
+        except ObjectDoesNotExist as e:
+            print('Comment does not exist', str(e))
+
         # 상세페이지로 돌아가기
         post_id = hashtag.post.id
         # 삭제
---
+++ Django실습/restapp/user/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils import timezone
+
+# Create your models here.
+'''
+Auth User model
+- 생성
+- 삭제
+- 수정
+--> UserManager helper class 도움주는 클래스
+'''
+
+'''
+Django 자체에서 UserManager는 유저 생성을 도와주는 매니저 클래스입니다.
+이 클래스에서는 create_user(), create_superuser() 두 메서드가 제공되는데
+수업에서는 이를 우리가 수정한 User 모델에 맞춰 변경해주었습니다.
+_create_user()는 필수 필드를 검증한 다음
+create_user(), create_superuser()로 구분해서 사용자와 관리자를 생성할 수 있습니다.
+'''
+class UserManager(BaseUserManager):
+    
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+        # User모델의 username이 아니라 email을 id처럼 사용하기 위해서 email이 필수임을 체크합니다. -> 아닐시 오류를 발생시켜 유저 생성이 진행되지 않습니다.
+        if not email:
+            raise ValueError('User must have an email')
+        # now = timezone.now() # 현재시간 -> UTC
+        now = timezone.localtime()
+        # User 테이블에 타입(class User(AbstractUser) 내부의 last_login, date_joined 필드)에 맞춰 현재 시각을 가져오기 위한 부분입니다. (데이터 타입: datetime)
+        email = self.normalize_email(email) 
+        # normalize_email은 BaseUserManager에서 제공하는 메서드로 정규화를 실행하는 메서드(함수)입니다.
+        # 이메일 주소의 대소문자 구분에 따른 중복계정 방지를 위해 사용됩니다.
+        user = self.model(
+            email=email,
+            is_staff=is_staff,
+            is_active=True,
+            is_superuser=is_superuser,
+            last_login=now,
+            date_joined=now,
+            **extra_fields
+        ) # 일반 유저인지 관리자 유저인지를 메서드 실행시 입력받은 값으로 구분해서 유저를 생성합니다.
+        user.set_password(password) # 여기서 set_password 메서드는 사용자에게 받은 암호를 안전하게 저장하기 위해 암호화 과정을 더해주는 부분입니다.
+        user.save(using=self._db) # using는 어떤 데이터베이스를 사용할 지 지정해주는 매개변수로 self._db는 현재 사용중인 데이터베이스를 의미합니다.
+        return user
+    # create_user
+    def create_user(self, email, password, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
+    # create_superuser
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, max_length=255)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
+    
+    # def __str__(self):
+    #     return self.name
